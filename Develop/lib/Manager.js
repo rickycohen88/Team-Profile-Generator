3f5326c352654eeb83259d3bb62a065efb3dce86
RickyCohen <ricky.cohen88@gmail.com>
2020-12-08 01:16:29 -0600

    created local repo, then got carried away and creatyed all the classes/constructors

---
+++ Develop/lib/Manager.js
@@ -0,0 +1,16 @@
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("Employee.js");
+
+class Manager extends Employee{
+    constructor(name,id,email,offNum){
+        super(name,id,email)
+        this.officeNumber = offNum;
+        this.role = this.constructor.name;
+    }
+    getRole(){
+        return this.role;
+    }
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+}
